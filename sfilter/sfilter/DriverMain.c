73b9aa86e3cd155765d1eef5a1c8dc2cf26a5dcf
xiaobo93 <guoxiaobo93@163.com>
2019-09-01 23:08:01 +0800

    文件系统卷的绑定

--- sfilter/sfilter/DriverMain.c
+++ sfilter/sfilter/DriverMain.c
@@ -7,6 +7,8 @@
 
 PDEVICE_OBJECT gSFilterControlDeivceObject = NULL;
 PDRIVER_OBJECT gSFilterDriverObject = NULL;
+SF_DYNAMIC_FUNCTION_POINTERS gSfDynamicFunctions = { 0 }; //存放导出函数的位置
+
 
 VOID DriverUnload(PDRIVER_OBJECT pDriverObject)
 {
@@ -19,7 +21,6 @@ NTSTATUS SfGetObjectName(IN PVOID Object, OUT POBJECT_NAME_INFORMATION * name)
 	返回给定对象的名称，如果对象没有，返回一个空字符串
 ++*/
 {
-	_asm int 3;
 	NTSTATUS ntStatus = STATUS_SUCCESS;
 	ULONG retLength;
 	POBJECT_NAME_INFORMATION tmp;
@@ -41,12 +42,170 @@ NTSTATUS SfGetObjectName(IN PVOID Object, OUT POBJECT_NAME_INFORMATION * name)
 SfGetObjectName_Exit:
 	return ntStatus;
 }
+BOOLEAN SfIsAttachedToDevice(
+	PDEVICE_OBJECT DeviceObject
+)
+{
+	PDEVICE_OBJECT currentDevObj;
+	PDEVICE_OBJECT nextDevObj;
+
+	if (gSfDynamicFunctions.GetAttachedDeviceReference != NULL)
+	{
+		do {
+
+			if (currentDevObj->DriverObject == gSFilterDriverObject &&
+				currentDevObj->DeviceExtension != NULL)
+			{
+				ObDereferenceObject(currentDevObj);
+				return TRUE;
+			}
+			if (gSfDynamicFunctions.GetLowerDeviceObject == NULL)
+			{
+				ObDereferenceObject(currentDevObj);
+				return FALSE;
+			}
+			nextDevObj = (gSfDynamicFunctions.GetLowerDeviceObject)(currentDevObj);
+			ObDereferenceObject(currentDevObj);
+			currentDevObj = nextDevObj;
+
+		} while (currentDevObj != NULL);
+	}
+	return FALSE;
+}
+NTSTATUS SfEnumerateFileSystemVolumes(IN PDEVICE_OBJECT FSDeviceObject, IN PUNICODE_STRING Name)
+{
+	NTSTATUS ntStatus = STATUS_SUCCESS;
+	ULONG numDevices;
+	PDEVICE_OBJECT newDeviceObject;
+	PSFILTER_DEVICE_EXTENSION newDevExt;
+	PDEVICE_OBJECT storageStackDeviceObject;
+	ULONG i;
+	BOOLEAN isShadowCopyVolume;
+	PDEVICE_OBJECT *devList;
+
+	if (gSfDynamicFunctions.EnumerateDeviceObjectList != NULL)
+	{
+		ntStatus = (gSfDynamicFunctions.EnumerateDeviceObjectList)(
+			FSDeviceObject->DriverObject,
+			NULL,
+			0,
+			&numDevices
+			);
+		if (ntStatus == STATUS_BUFFER_TOO_SMALL)
+		{
+			numDevices += 8;
+			devList = ExAllocatePoolWithTag(NonPagedPool, (numDevices * sizeof(PDEVICE_OBJECT)),
+				'xb');
+			if (NULL == devList)
+			{
+				return STATUS_INSUFFICIENT_RESOURCES;
+			}
+			ntStatus = (gSfDynamicFunctions.EnumerateDeviceObjectList)(
+				FSDeviceObject->DriverObject,
+				devList,
+				(numDevices * sizeof(PDEVICE_OBJECT)),
+				&numDevices
+				);
+			if (!NT_SUCCESS(ntStatus))
+			{
+				ExFreePool(devList);
+				return ntStatus;
+			}
+			for (i = 0; i < numDevices; i++)
+			{
+				storageStackDeviceObject = NULL;
+				if (devList[i] == FSDeviceObject ||
+					(devList[i]->DeviceType != FSDeviceObject->DeviceType) ||
+					SfIsAttachedToDevice(devList[i]->DeviceType))
+				{//设备已经被绑定，不会再进行绑定
+					ObReferenceObject(devList[i]);
+					continue;
+				}
+			}
+		}
+	}
+	return ntStatus;
+}
+NTSTATUS sfAttachToFileSystemDevices(
+	IN PDEVICE_OBJECT DeviceObject,
+	IN PUNICODE_STRING DeviceName
+)
+{
+	PSFILTER_DEVICE_EXTENSION devExt = NULL;
+	PDEVICE_OBJECT newDeviceObject = NULL;
+	NTSTATUS status = STATUS_SUCCESS;
+	if (DeviceObject->DeviceType != FILE_DEVICE_DISK_FILE_SYSTEM ||
+		DeviceObject->DeviceType != FILE_DEVICE_CD_ROM ||
+		DeviceObject->DeviceType != FILE_DEVICE_NETWORK_FILE_SYSTEM)
+	{
+		return STATUS_SUCCESS;
+	}
+	if (wcsicmp(L"\\FileSystem\\Fs_Rec", DeviceObject->DriverObject->DriverName.Buffer) == 0)
+	{
+		return STATUS_SUCCESS;
+	}
+	status = IoCreateDevice(gSFilterDriverObject,
+		sizeof(SFILTER_DEVICE_EXTENSION),
+		NULL,
+		DeviceObject->DeviceType,
+		0,
+		FALSE,
+		&newDeviceObject);
+	if (!NT_SUCCESS(status))
+	{
+		return status;
+	}
+	if (FlagOn(DeviceObject->Flags, DO_BUFFERED_IO))
+	{
+		SetFlag(newDeviceObject->Flags, DO_BUFFERED_IO);
+	}
+	if (FlagOn(DeviceObject->Flags, DO_DIRECT_IO))
+	{
+		SetFlag(newDeviceObject->Flags, DO_DIRECT_IO);
+	}
+	if (FlagOn(DeviceObject->Characteristics, FILE_DEVICE_SECURE_OPEN))
+	{
+		SetFlag(newDeviceObject->Characteristics, FILE_DEVICE_SECURE_OPEN);
+	}
+	devExt = newDeviceObject->DeviceExtension;
+	devExt->AttachedToDeviceObject =  IoAttachDeviceToDeviceStack(newDeviceObject, DeviceObject);
+	if (devExt->AttachedToDeviceObject == NULL)
+	{
+		KdPrint(("%s 文件系统(%ws)绑定失败\n", __FUNCTION__, DeviceObject->DriverObject->DriverName.Buffer));
+		goto sfAttachToFileSystemDevices_exit;
+	}
+	else {
+		KdPrint(("%s 文件系统(%ws)绑定成功\n", __FUNCTION__, DeviceObject->DriverObject->DriverName.Buffer));
+	}
+	if (wcscpy_s(devExt->DeviceNameBuffer, sizeof(devExt->DeviceNameBuffer),
+		DeviceObject->DriverObject->DriverName.Buffer) != 0)
+	{
+		KdPrint(("%s设备名称拷贝失败\n",__FUNCTION__));
+	}
+	ClearFlag(newDeviceObject->Flags, DO_DEVICE_INITIALIZING);
+	//遍历绑定，文件系统上的卷信息。
+
+	return STATUS_SUCCESS;
+
+sfAttachToFileSystemDevices_exit:
+	IoDeleteDevice(newDeviceObject);
+	return STATUS_SUCCESS;
+}
 VOID SfFsNotification(IN PDEVICE_OBJECT DeviceObject,IN BOOLEAN FsActive)
 {//监测新创建的设备信息。
 	NTSTATUS ntStatus;
 	POBJECT_NAME_INFORMATION name = NULL;
 	ntStatus = SfGetObjectName(DeviceObject, &name);
-	KdPrint(("磁盘名称为 %ws\n", name->Name.Buffer));
+	KdPrint(("%s磁盘名称为 %ws type : 0x%x\n",__FUNCTION__, name->Name.Buffer, DeviceObject->DeviceType));
+	_asm int 3;
+	if (FsActive)
+	{//绑定文件系统
+		sfAttachToFileSystemDevices(DeviceObject,&name->Name);
+	}
+	else
+	{//卸载文件系统
+
+	}
 	if (name != NULL)
 	{
 		ExFreePool(name);
@@ -66,6 +225,35 @@ NTSTATUS DeviceDispatch(IN PDEVICE_OBJECT DeviceObject,IN PIRP Irp)
 	}
 	return ntStatus;
 }
+VOID SfLoadDynamicFunctions()
+{
+	UNICODE_STRING functionName;
+	RtlZeroMemory(&gSfDynamicFunctions, sizeof(gSfDynamicFunctions));
+	RtlInitUnicodeString(&functionName, L"FsRtlRegisterFileSystemFilterCallbacks");
+	gSfDynamicFunctions.RegisterFileSystemFilterCallbacks = MmGetSystemRoutineAddress(&functionName);
+
+	RtlInitUnicodeString(&functionName, L"IoAttachDeviceToDeviceStackSafe");
+	gSfDynamicFunctions.AttachDeviceToDeviceStackSafe = MmGetSystemRoutineAddress(&functionName);
+
+	RtlInitUnicodeString(&functionName, L"IoEnumerateDeviceObjectList");
+	gSfDynamicFunctions.EnumerateDeviceObjectList = MmGetSystemRoutineAddress(&functionName);
+
+	RtlInitUnicodeString(&functionName, L"IoGetLowerDeviceObject");
+	gSfDynamicFunctions.GetLowerDeviceObject = MmGetSystemRoutineAddress(&functionName);
+
+	RtlInitUnicodeString(&functionName, L"IoGetDeviceAttachmentBaseRef");
+	gSfDynamicFunctions.GetDeviceAttachmentBaseRef = MmGetSystemRoutineAddress(&functionName);
+
+	RtlInitUnicodeString(&functionName, L"IoGetDiskDeviceObject");
+	gSfDynamicFunctions.GetDiskDeviceObject = MmGetSystemRoutineAddress(&functionName);
+
+	RtlInitUnicodeString(&functionName, L"IoGetAttachedDeviceReference");
+	gSfDynamicFunctions.GetAttachedDeviceReference = MmGetSystemRoutineAddress(&functionName);
+
+	RtlInitUnicodeString(&functionName, L"RtlGetVersion");
+	gSfDynamicFunctions.GetVersion = MmGetSystemRoutineAddress(&functionName);
+}
+
 NTSTATUS
 DriverEntry(
 	IN PDRIVER_OBJECT DriverObject,
@@ -74,10 +262,15 @@ DriverEntry(
 {
 	NTSTATUS status = STATUS_SUCCESS;
 	UNICODE_STRING nameString;
+	PFAST_IO_DISPATCH fastIoDispatch = NULL;
+
 	gSFilterDriverObject = DriverObject;
 	RtlInitUnicodeString(&nameString, L"\\FileSystem\\Filters\\SFilterCDO");
 	DriverObject->DriverUnload = DriverUnload;
-	PFAST_IO_DISPATCH fastIoDispatch = NULL;
+
+	//加载没有导出的函数
+	SfLoadDynamicFunctions();
+
 	status = IoCreateDevice(DriverObject,
 		0,
 		&nameString,
--- sfilter/sfilter/FsHeader.h
+++ sfilter/sfilter/FsHeader.h
@@ -22,12 +22,6 @@ typedef struct _SFILTER_DEVICE_EXTENSION {
 	//  Object it is the name of the Control Device Object.
 	//
 
-	UNICODE_STRING DeviceName;
-
-	//
-	//  Buffer used to hold the above unicode strings
-	//
-
 	WCHAR DeviceNameBuffer[64];
 
 	// 
@@ -37,3 +31,75 @@ typedef struct _SFILTER_DEVICE_EXTENSION {
 	UCHAR UserExtension[1];
 
 } SFILTER_DEVICE_EXTENSION, *PSFILTER_DEVICE_EXTENSION;
+
+typedef
+NTSTATUS
+(*PSF_REGISTER_FILE_SYSTEM_FILTER_CALLBACKS) (
+	IN PDRIVER_OBJECT DriverObject,
+	IN PFS_FILTER_CALLBACKS Callbacks
+	);
+
+typedef
+NTSTATUS
+(*PSF_ENUMERATE_DEVICE_OBJECT_LIST) (
+	IN  PDRIVER_OBJECT DriverObject,
+	IN  PDEVICE_OBJECT *DeviceObjectList,
+	IN  ULONG DeviceObjectListSize,
+	OUT PULONG ActualNumberDeviceObjects
+	);
+
+typedef
+NTSTATUS
+(*PSF_ATTACH_DEVICE_TO_DEVICE_STACK_SAFE) (
+	IN PDEVICE_OBJECT SourceDevice,
+	IN PDEVICE_OBJECT TargetDevice,
+	OUT PDEVICE_OBJECT *AttachedToDeviceObject
+	);
+
+typedef
+PDEVICE_OBJECT
+(*PSF_GET_LOWER_DEVICE_OBJECT) (
+	IN  PDEVICE_OBJECT  DeviceObject
+	);
+
+typedef
+PDEVICE_OBJECT
+(*PSF_GET_DEVICE_ATTACHMENT_BASE_REF) (
+	IN PDEVICE_OBJECT DeviceObject
+	);
+
+typedef
+NTSTATUS
+(*PSF_GET_DISK_DEVICE_OBJECT) (
+	IN  PDEVICE_OBJECT  FileSystemDeviceObject,
+	OUT PDEVICE_OBJECT  *DiskDeviceObject
+	);
+
+typedef
+PDEVICE_OBJECT
+(*PSF_GET_ATTACHED_DEVICE_REFERENCE) (
+	IN PDEVICE_OBJECT DeviceObject
+	);
+
+typedef
+NTSTATUS
+(*PSF_GET_VERSION) (
+	IN OUT PRTL_OSVERSIONINFOW VersionInformation
+	);
+typedef struct _SF_DYNAMIC_FUNCTION_POINTERS {
+
+	//
+	//  The following routines should all be available on Windows XP (5.1) and
+	//  later.
+	//
+
+	PSF_REGISTER_FILE_SYSTEM_FILTER_CALLBACKS RegisterFileSystemFilterCallbacks;
+	PSF_ATTACH_DEVICE_TO_DEVICE_STACK_SAFE AttachDeviceToDeviceStackSafe;
+	PSF_ENUMERATE_DEVICE_OBJECT_LIST EnumerateDeviceObjectList;
+	PSF_GET_LOWER_DEVICE_OBJECT GetLowerDeviceObject;
+	PSF_GET_DEVICE_ATTACHMENT_BASE_REF GetDeviceAttachmentBaseRef;
+	PSF_GET_DISK_DEVICE_OBJECT GetDiskDeviceObject;
+	PSF_GET_ATTACHED_DEVICE_REFERENCE GetAttachedDeviceReference;
+	PSF_GET_VERSION GetVersion;
+
+} SF_DYNAMIC_FUNCTION_POINTERS, *PSF_DYNAMIC_FUNCTION_POINTERS;
